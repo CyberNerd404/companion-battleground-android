1ba9cbd08885d9188e7ccee1021193e6bf16c839
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-05-16 17:17:18 +0530

    added news activty

--- app/src/main/java/com/cybernerd/companionBattleground/view/home/news/HomeNewsFragment.kt
+++ app/src/main/java/com/cybernerd/companionBattleground/view/home/news/HomeNewsFragment.kt
@@ -1,26 +1,27 @@
 package com.cybernerd.companionBattleground.view.home.news
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cybernerd.companionBattleground.R
-import com.cybernerd.companionBattleground.adapter.HomeCardViewAdapter
-import com.cybernerd.companionBattleground.model.HomeCardViewModel
+import com.cybernerd.companionBattleground.adapter.HomeNewsAdapter
+import com.cybernerd.companionBattleground.model.HomeNewsModel
+import com.cybernerd.companionBattleground.model.HomeVideoModel
 import com.cybernerd.companionBattleground.utils.ClickListener
+import com.cybernerd.companionBattleground.utils.debug
 import com.cybernerd.companionBattleground.view.BaseFragment
-import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home_news.*
 
 
 class HomeNewsFragment : BaseFragment(), ClickListener {
 
 
-    lateinit var homeAdapter: HomeCardViewAdapter
-    lateinit var homeItemList: MutableList<HomeCardViewModel>
-    lateinit var homeModel: HomeCardViewModel
+    lateinit var homeAdapter: HomeNewsAdapter
+    lateinit var homeItemList: MutableList<HomeNewsModel>
+    lateinit var homeModel: HomeNewsModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +34,11 @@ class HomeNewsFragment : BaseFragment(), ClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        homeAdapter = HomeCardViewAdapter(requireContext(), this)
+        homeAdapter = HomeNewsAdapter(requireContext(), this)
         home_news_rv.adapter = homeAdapter
 
         homeItemList = arrayListOf()
-        homeModel = HomeCardViewModel()
+        homeModel = HomeNewsModel()
 
         home_news_rv.layoutManager = GridLayoutManager(requireContext(), 2)
 
@@ -47,29 +48,50 @@ class HomeNewsFragment : BaseFragment(), ClickListener {
     private fun setHomeCardView() {
         homeModel.image = R.drawable.battleground_logo
         homeModel.name = "Testing 1"
-        homeItemList.add(homeModel)
+        homeItemList.add(0, homeModel)
 
-        homeModel.image = R.drawable.battleground_logo
+        homeModel = HomeNewsModel()
+        homeModel.image = R.drawable.pubg_logo
         homeModel.name = "Testing 2"
-        homeItemList.add(homeModel)
+        homeItemList.add(1, homeModel)
 
-        homeModel.image = R.drawable.battleground_logo
+        homeModel = HomeNewsModel()
+        homeModel.image = R.drawable.logo_two
         homeModel.name = "Testing 3"
-        homeItemList.add(homeModel)
+        homeItemList.add(2, homeModel)
 
+        homeModel = HomeNewsModel()
         homeModel.image = R.drawable.battleground_logo
         homeModel.name = "Testing 4"
-        homeItemList.add(homeModel)
+        homeItemList.add(3, homeModel)
 
-        homeModel.image = R.drawable.battleground_logo
+        homeModel = HomeNewsModel()
+        homeModel.image = R.drawable.logo_two
         homeModel.name = "Testing 5"
-        homeItemList.add(homeModel)
+        homeItemList.add(4, homeModel)
 
-        homeModel.image = R.drawable.battleground_logo
+        homeModel = HomeNewsModel()
+        homeModel.image = R.drawable.pubg_logo
         homeModel.name = "Testing 6"
-        homeItemList.add(homeModel)
+        homeItemList.add(5, homeModel)
+
+        debug(" size : ${homeItemList.size} homeItemList:$homeItemList")
 
         homeAdapter.setHomeCardView(homeItemList)
+    }
+
+    override fun homeNewsClickListener(homeNewsModel: HomeNewsModel) {
+        activity.let {
+            Intent(it, NewsActivity::class.java).apply {
+                putExtra("name", homeNewsModel.name)
+                putExtra("img", homeNewsModel.image)
+                startActivity(this)
+            }
+        }
+    }
+
+    override fun homeVideoClickListener(homeVideoModel: HomeVideoModel) {
+        TODO("Not yet implemented")
     }
 
 
