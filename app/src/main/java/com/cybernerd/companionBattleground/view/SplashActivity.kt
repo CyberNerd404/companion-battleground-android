a463adae6b413daab8bb49efedbfe9e98f79d802
jasim.ahmed <jasim.ahmed@diatoz.com>
2021-05-16 18:42:43 +0530

    Added code for clearing back stack.

--- app/src/main/java/com/cybernerd/companionBattleground/view/SplashActivity.kt
+++ app/src/main/java/com/cybernerd/companionBattleground/view/SplashActivity.kt
@@ -1,10 +1,9 @@
 package com.cybernerd.companionBattleground.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.cybernerd.companionBattleground.R
 
 class SplashActivity : AppCompatActivity() {
@@ -13,9 +12,10 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            Intent(this, MainActivity::class.java).apply {
-                startActivity(this)
-            }
-        }, 3000)
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            this@SplashActivity.finish()
+        }, 2500)
     }
 }
