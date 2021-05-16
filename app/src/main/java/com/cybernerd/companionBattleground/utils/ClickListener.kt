58cf043ec38f654f23ec97edad15982ecf40dde0
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-05-16 17:16:49 +0530

    added extensions funtion and custom callback listener

--- app/src/main/java/com/cybernerd/companionBattleground/utils/ClickListener.kt
+++ app/src/main/java/com/cybernerd/companionBattleground/utils/ClickListener.kt
@@ -1,4 +1,14 @@
 package com.cybernerd.companionBattleground.utils
 
+import com.cybernerd.companionBattleground.model.HomeNewsModel
+import com.cybernerd.companionBattleground.model.HomeVideoModel
+
 interface ClickListener {
+
+
+    // click listener for news recyclerview
+    fun homeNewsClickListener(homeNewsModel: HomeNewsModel)
+
+    // click listener for youtude recyclerview
+    fun homeVideoClickListener(homeVideoModel: HomeVideoModel)
 }
