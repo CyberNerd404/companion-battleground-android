c440112e315b7f17da4a8434aafd264ed9f7499e
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-05-18 02:13:15 +0530

    get progressBar live data in viewModel and observed on view

--- app/src/main/java/com/cybernerd/companionBattleground/view/home/topvideos/HomeVideoViewModel.kt
+++ app/src/main/java/com/cybernerd/companionBattleground/view/home/topvideos/HomeVideoViewModel.kt
@@ -2,6 +2,7 @@ package com.cybernerd.companionBattleground.view.home.topvideos
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cybernerd.companionBattleground.model.HomeVideosModel
@@ -11,8 +12,11 @@ class HomeVideoViewModel: ViewModel() {
 
     val homeVideoRepository = HomeVideoRepository()
     var videoLiveData = MutableLiveData<HomeVideosModel>()
+    val showprogress : LiveData<Boolean>
+
 
     init {
+        this.showprogress = homeVideoRepository.showProgress
         this.videoLiveData = homeVideoRepository.videoLiveData
     }
 
