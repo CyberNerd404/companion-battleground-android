4342756f973ce30aa8ad2252fbfb56431506a316
jasim.ahmed <jasim.ahmed@diatoz.com>
2021-05-18 21:37:18 +0530

    Designed UI for News Activity.

--- app/src/main/java/com/cybernerd/companionBattleground/view/home/news/NewsActivity.kt
+++ app/src/main/java/com/cybernerd/companionBattleground/view/home/news/NewsActivity.kt
@@ -1,9 +1,13 @@
 package com.cybernerd.companionBattleground.view.home.news
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.cybernerd.companionBattleground.R
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import kotlinx.android.synthetic.main.activity_news.*
+
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +17,16 @@ class NewsActivity : AppCompatActivity() {
         val intent = intent.extras
 
         val imv = intent?.getInt("img")
+//        val toolbar_layout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+        collapsing_toolbar.setExpandedTitleColor(Color.WHITE);
 
-        if (imv != null) {
-            news_iv.setImageResource(imv)
-        }
-
-
-        news_back_iv.setOnClickListener {
-            finish()
-        }
+//        if (imv != null) {
+//            news_iv.setImageResource(imv)
+//        }
+//
+//
+//        news_back_iv.setOnClickListener {
+//            finish()
+//        }
     }
 }
