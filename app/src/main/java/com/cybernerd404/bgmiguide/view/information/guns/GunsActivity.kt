97bcac0931ed5af47272575a701f499dedd7d747
ashutoshwahane <ashutosh.wahane10@gmail.com>
2021-06-27 14:55:39 +0530

    fixed gun activity added floating action button

--- app/src/main/java/com/cybernerd404/bgmiguide/view/information/guns/GunsActivity.kt
+++ app/src/main/java/com/cybernerd404/bgmiguide/view/information/guns/GunsActivity.kt
@@ -1,20 +1,45 @@
 package com.cybernerd404.bgmiguide.view.information.guns
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cybernerd404.bgmiguide.R
+import com.cybernerd404.bgmiguide.adapter.GunsAdapter
 import com.cybernerd404.bgmiguide.model.GunsModelX
+import kotlinx.android.synthetic.main.activity_guns.*
 
 class GunsActivity : AppCompatActivity() {
 
     lateinit var list: MutableList<GunsModelX>
-
+    lateinit var gunsAdapter: GunsAdapter
+    val viewModel: GunsViewModel by lazy {
+        GunsViewModel()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guns)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fame_layout, GunsFragment()).commit()
+        gunsAdapter = GunsAdapter(this)
+
+        guns_rv.adapter = gunsAdapter
+        guns_rv.layoutManager = LinearLayoutManager(this)
+
+        viewModel.getGuns()
+        viewModel.gunsLiveData.observe(this, Observer {
+            if (it != null)
+                gunsAdapter.setGuns(it)
+        })
+
+        viewModel.showprogress.observe(this, Observer {
+            if (it) {
+                guns_progress.visibility = View.VISIBLE
+            } else {
+                guns_progress.visibility = View.GONE
+            }
+        })
 
     }
 
